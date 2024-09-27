685e0e689ddce96dc3c84653dc7b600e2b6bcbd2
Stephen Lee <leestephen0320@gmail.com>
2024-09-27 00:02:16 -0700

    finished todos for wheelie functions

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -399,15 +399,25 @@ class Cli {
         // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
         else if (answers.action == 'Tow') {
           for (let i = 0; i < this.vehicles.length; i++) {
-            if (this.vehicles[i].vin === this.selectedVehicleVin) {
-              this.findVehicleToTow(this.vehicles[i]);
-              return;
+            if (this.vehicles[i] instanceof Truck){
+              if (this.vehicles[i].vin === this.selectedVehicleVin) {
+                this.findVehicleToTow(this.vehicles[i] as Truck);
+                return;
+              }
             }
           }
         }
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
         else if (answers.action == 'Wheelie') {
-
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i] instanceof Motorbike){
+              if (this.vehicles[i].vin === this.selectedVehicleVin) {
+                const motorWheelie = this.vehicles[i] as Motorbike;
+                motorWheelie.wheelie();
+                return;
+              }
+            }
+          }
         }
         else if (answers.action === 'Select or create another vehicle') {
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
